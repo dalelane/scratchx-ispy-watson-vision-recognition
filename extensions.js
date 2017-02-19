bc4ff87c7f81355d20d722705cd84da1e833304b
Dale Lane <dale.lane@uk.ibm.com>
2017-02-19 00:02:22 +0000

    first commit

---
+++ extensions.js
@@ -0,0 +1,47 @@
+(function(ext) {
+
+    ext.resultscache = {};
+
+    ext._shutdown = function() {};
+
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.classify_image = function (location, callback) {
+      $.ajax({
+        url: 'https://scratch-ispy.eu-gb.mybluemix.net/recognise?url=' + location,
+        dataType: 'jsonp',
+        success: function( myresults ) {
+          console.log(myresults);
+
+          ext.resultscache[location] = myresults;
+          callback(myresults.length);
+        },
+        error: function (err) {
+          console.log(err);
+          callback([]);
+        }
+      });
+    };
+
+    ext.get_image_class = function (idx, location) {
+      if (ext.resultscache[location] && ext.resultscache[location].length > idx) {
+        return ext.resultscache[location][idx].name;
+      }
+      else {
+        return '';
+      }
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', 'recognise objects in %s', 'classify_image', 'url'],
+            ['r', 'get recognised object %d from %s', 'get_image_class', 0, 'url']
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Watson Vision Recognition', descriptor, ext);
+})({});
